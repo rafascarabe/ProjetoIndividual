2b96e85bf89758ef1e0525922c07f52b8b4273e6
rafascarabe <rafaela.scarabe@sptech.school>
2023-11-23 12:43:16 -0300

    script do bancdo de dados

--- Database/Script-projetoINDIVIDUAL.sql
+++ Database/Script-projetoINDIVIDUAL.sql
@@ -14,8 +14,33 @@ CREATE TABLE newsletter(
 idAssinante INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(50),
 email VARCHAR(45) NOT NULL UNIQUE,
-fkUSUARIO INT,
+fkUSUARIO INT, -- não é obrigatorio preencher 
 	CONSTRAINT newsletter FOREIGN KEY (fkUSUARIO) REFERENCES usuario(idUsuario)
 );
 
+CREATE TABLE livro (
+idLivro INT PRIMARY KEY AUTO_INCREMENT,
+titulo VARCHAR(45),
+autor VARCHAR(45),
+genero VARCHAR(45),
+resenha VARCHAR(1000),
+avaliacao DECIMAL(2,1),
+ CONSTRAINT CHECKavaliacao CHECK (avaliacao <= 5 and avaliacao >= 1)
+ );
 
+CREATE TABLE favoritados (
+fkUSUARIO INT,
+fkLIVRO INT,
+PRIMARY KEY (fkUSUARIO, fkLIVRO),
+	CONSTRAINT favoritados FOREIGN KEY (fkUSUARIO) REFERENCES usuario(idUsuario),
+    CONSTRAINT favoritado FOREIGN KEY (fkLIVRO) REFERENCES livro(idLivro)
+);
+
+CREATE TABLE comentario (
+idComentario INT PRIMARY KEY AUTO_INCREMENT,
+forum VARCHAR(45),
+fkUSUARIO INT,
+comentario VARCHAR(500),
+dataHora DATETIME,
+	CONSTRAINT comentario FOREIGN KEY (fkUSUARIO) REFERENCES usuario(idUsuario)
+);
